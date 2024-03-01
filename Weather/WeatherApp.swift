1c8d738c0e70ec490e0b32ee272bc3ea5922ffd9
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-03-01 15:45:54 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Turgut Eren Aksu on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
