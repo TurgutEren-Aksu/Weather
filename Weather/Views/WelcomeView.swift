1ef5d889c3c18c6aea244d23f860c483a98fbae7
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-03-08 13:50:55 +0300

    add CoreLocationUI

--- Weather/Views/WelcomeView.swift
+++ Weather/Views/WelcomeView.swift
@@ -6,17 +6,28 @@
 //
 
 import SwiftUI
+import CoreLocationUI
 
 struct WelcomeView: View {
 	@EnvironmentObject var locationManager:CoreLocation
     var body: some View {
 		VStack{
 			VStack{
-				Text("Welcome to the weather app")
-				Text("testtesttest")
+				Text("Hava Durumu Uygulamasına Hoş Geldiniz")
+					.bold().font(.title)
+				Text("Bölgenizdeki hava durumunu öğrenmek için lütfen konumunuzu paylaşın")
+					.padding()
 			}
 			.multilineTextAlignment(.center)
 			.padding()
+			
+			LocationButton(.shareCurrentLocation){
+				locationManager.requestLocation()
+			}
+			.cornerRadius(60)
+			.symbolVariant(.fill)
+			.foregroundColor(.white)
+			
 		}
 		.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
