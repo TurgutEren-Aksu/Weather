b0594adf0b5bda9b34b52593c7ba2b645caab7bd
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-03-12 14:27:57 +0300

    add WeatherManager

---
+++ Weather/Managers/WeatherManager.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherManager.swift
+//  Weather
+//
+//  Created by Turgut Eren Aksu on 12.03.2024.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherManager {
+	func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws {
+		
+	}
+}
