b950e1a165bb1dfdca5e2cfb759a482339944c2a
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-03-27 14:41:47 +0300

    Extensions

---
+++ Weather/Managers/Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Extensions.swift
+//  Weather
+//
+//  Created by Turgut Eren Aksu on 27.03.2024.
+//
+
+import Foundation
+extension Double{
+	func roundDouble() -> String{
+		return String(format: "%.0f", self)
+	}
+}
