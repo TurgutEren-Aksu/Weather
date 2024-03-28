e828f3199e215d71712e14f2c3417658e293bd1a
Turgut Eren Aksu <turguterenaksu@outlook.com>
2024-03-28 10:35:23 +0300

    UI

--- Weather/Views/WeatherView.swift
+++ Weather/Views/WeatherView.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 struct WeatherView: View {
 	var weather: ResponseBody
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+	var body: some View {
+		ZStack(alignment: .leading){
+			VStack{
+				VStack(alignment: .leading, spacing: 5){
+					Text(weather.name)
+						.bold().font(.title)
+					Text("Bugün \(Date().formatted(.dateTime.month().day().hour().minute()))").fontWeight(.light)
+					
+				}.frame(maxWidth: .infinity, alignment: .leading )
+				Spacer()
+				
+			}.padding()
+				.frame(maxWidth: .infinity, alignment: .leading)
+			
+		}.edgesIgnoringSafeArea(.bottom)
+			.background(Color(hue: 1.0, saturation: 0.500, brightness: 0.300))
+			.preferredColorScheme(.dark)
+	}
 }
 
 #Preview {
